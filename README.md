1564438a23e2e3d8bcd792c6e474dfdfb363461b
pikruse <pikruser@gmail.com>
2024-10-18 12:06:44 -0400

    updated GetLowestGPU

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The `DGXutils` package contains various functions for use on the DGX computing c
 * **`GetLowestGPU()`:** This function returns the GPU with the lowest memory use on the cluster. Returns a device string (e.g. 'cuda:0' or 'cpu' if no cuda devices).
 
 * **`GetFileNames()`:** This function returns a list of file names with a given extension from the specified directory.
+
+## Installation
--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,8 @@ from DGXutils import GetLowestGPU, GetFileNames
 
 class TestDGXutils(unittest.TestCase):
     def test_GetLowestGPU(self):
-        self.assertEqual(GetLowestGPU() in ['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3', 'cuda:4', 'cuda:5', 'cuda:6', 'cuda:7', 'cpu'], True)
+        out = GetLowestGPU()
+        self.assertTrue(out == 'cpu' or out.startswith('cuda:'))
 
     def test_GetFileNames(self):
         self.assertEqual(GetFileNames('test_dir', '.txt'), ["test.txt"])
